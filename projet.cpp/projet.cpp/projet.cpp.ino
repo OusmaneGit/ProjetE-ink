689c021ed9f8b8401c586e32fa56a30d465c5dc2
Ba <ba.ousmane@satadsl.net>
2022-02-25 23:46:12 +0100

    Refactoring code

--- projet.cpp/projet.cpp/projet.cpp.ino
+++ projet.cpp/projet.cpp/projet.cpp.ino
@@ -1,33 +1,37 @@
-// include library, include base class, make path known
 #include <GxEPD.h>
 #include "SD.h"
 #include "SPI.h"
-//WIFI include
+
+
 #include <WiFi.h>
+#include "time.h"
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 //new API
 #include <Arduino_JSON.h>
-//Since there are multiple versions of the screen, if there is a flower screen after downloading the program, please test the following four header files again!
 
+//Provide your own WiFi credentials
+const char* ssid = "Proximus-Home-84B0";//Proximus-Home-84B0   LARAS
+const char* password = "w2eyafdmrh3re";//w2eyafdmrh3re   wifi4guest
+
+const char* ntpServer = "ntp.ntsc.ac.cn";//local ntp server
+const uint32_t  gmtOffset_sec = 8*3600;  //GMT+08:00
+const uint16_t   daylightOffset_sec = 0;
+
+
+//Since there are multiple versions of the screen, if there is a flower screen after downloading the program, please test the following four header files again!
 //#include <GxDEPG0213BN/GxDEPG0213BN.h>
 //#include <GxGDE0213B1/GxGDE0213B1.h>      // 2.13" b/w
 //#include <GxGDEH0213B72/GxGDEH0213B72.h>  // 2.13" b/w new panel
-//#include <GxGDEH0213B73/GxGDEH0213B73.h>  // 2.13" b/w newer panel
-//#include <GxGDEW0213I5F/GxGDEW0213I5F.h>
-#include <GxGDEW027W3/GxGDEW027W3.h>
-int bmpWidth = 232, bmpHeight = 52;
-//width:150,height:39
+#include <GxGDEH0213B73/GxGDEH0213B73.h>  // 2.13" b/w newer panel
 
-// FreeFonts from Adafruit_GFX
-#include <Fonts/FreeMonoBold9pt7b.h>
-#include <Fonts/FreeMonoBold12pt7b.h>
-#include <Fonts/FreeMonoBold18pt7b.h>
-#include <Fonts/FreeMonoBold24pt7b.h>
 //conversion
 #include <iostream>
 #include <string>
 //
+
+#include <Fonts/FreeMonoBold18pt7b.h>
+
 
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>
@@ -54,29 +58,24 @@ typedef enum
     LEFT_ALIGNMENT,
     CENTER_ALIGNMENT,
 } Text_alignment;
-//end of imports 
+
+
 
 GxIO_Class io(SPI, /*CS=5*/ ELINK_SS, /*DC=*/ ELINK_DC, /*RST=*/ ELINK_RESET);
 GxEPD_Class display(io, /*RST=*/ ELINK_RESET, /*BUSY=*/ ELINK_BUSY);
 
 SPIClass sdSPI(VSPI);
 
-//Provide your own WiFi credentials
-const char* ssid = "Proximus-Home-84B0";//Proximus-Home-84B0   LARAS
-const char* password = "w2eyafdmrh3re";//w2eyafdmrh3re   wifi4guest
-//String for storing server response
-
 const uint8_t Whiteboard[1700] = {0x00};
-
+//
 String response = "";
 //JSON document
 DynamicJsonDocument doc(2048);
 
 const char *Website = "Test API";
-bool sdOK = false;
+//bool sdOK = false;
 int startX = 9, startY = 20;
-
-
+//
 //Wheather API
 // Your Domain name with URL path or IP address with path
 String openWeatherMapApiKey = "b8890bd0a9fe6b0bccc79b716cbdcd2f";
@@ -98,61 +97,12 @@ unsigned long timerDelay = 10000;
 String jsonBuffer;
 //
 float Temp=0.0;
-//char Time[]={"00:00:00"};
-//end of whether API
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("setup");
 
-  Serial.printf("Connecting to %s ", ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println(" CONNECTED");
-  
-  //
-  Serial.begin(115200);
-  Serial.println();
-  Serial.println("setup");
-  //Wait two seconds for next joke
-  //
-  SPI.begin(SPI_CLK, SPI_MISO, SPI_MOSI, ELINK_SS);
-  display.init(); // enable diagnostic output on Serial
+uint16_t Year = 0 , Month = 0 , Day = 0 , Hour = 0 , Minute = 0 , Second = 0;
+char Date[]={"2000/01/01"};
+char Time[]={"00:00:00"};
+bool sdOK = false;
 
-  display.setRotation(1);
-  display.fillScreen(GxEPD_WHITE);
-  display.setTextColor(GxEPD_BLACK);
-  display.setFont(&FreeMonoBold18pt7b);
-  display.setCursor(0, 0);
-
-  sdSPI.begin(SDCARD_CLK, SDCARD_MISO, SDCARD_MOSI, SDCARD_SS);
-
-  if (!SD.begin(SDCARD_SS, sdSPI)) {
-    sdOK = false;
-  } else {
-    sdOK = true;
-  }
-
-  display.fillScreen(GxEPD_WHITE);
-  display.update();
-  
-}
-
-void loop()
-{
-  getTemp();
-  //displayText(String(Date), 60, CENTER_ALIGNMENT);
-  displayText(String(Temp), 90, CENTER_ALIGNMENT);
-   // Send an HTTP GET request
-  display.updateWindow(22, 30,  222,  90, true);
-  display.drawBitmap(Whiteboard, 22, 31,  208, 60, GxEPD_BLACK);
-  
-  //delay(8000);
-}
 void displayText(const String &str, uint16_t y, uint8_t alignment)
 {
   int16_t x = 0;
@@ -202,11 +152,57 @@ void getTemp()
   //Close connection  
   http.end();
   //display.print("Temp BXL:");
-  Temp=doc["main"]["temp"].as<float>()-273.15;
+  Temp=doc["main"]["temp"].as<float>()-273.15 ;
   //Temp=Temperature;
   //display.print(doc["main"]["temp"].as<float>()-273.15 );
   //display.print(" °C ");
   Serial.println(Temp);
   //Serial.println(Time);
   Serial.println(" ");
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("setup");
+
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+
+  SPI.begin(SPI_CLK, SPI_MISO, SPI_MOSI, ELINK_SS);
+  display.init(); // enable diagnostic output on Serial
+
+  display.setRotation(1);
+  display.fillScreen(GxEPD_WHITE);
+  display.setTextColor(GxEPD_BLACK);
+  display.setFont(&FreeMonoBold18pt7b);
+  display.setCursor(0, 0);
+
+  sdSPI.begin(SDCARD_CLK, SDCARD_MISO, SDCARD_MOSI, SDCARD_SS);
+
+  if (!SD.begin(SDCARD_SS, sdSPI)) {
+    sdOK = false;
+  } else {
+    sdOK = true;
+  }
+
+  display.fillScreen(GxEPD_WHITE);
+  display.update();
+}
+
+void loop()
+{
+  getTemp();
+  displayText(String(Temp)+ " C" , 60, CENTER_ALIGNMENT);
+  displayText(String("BXL"), 90, CENTER_ALIGNMENT);
+   // Send an HTTP GET request
+  display.updateWindow(22, 30,  222,  90, true);
+  display.drawBitmap(Whiteboard, 22, 31,  208, 60, GxEPD_BLACK);
+  delay(5000);
 }
